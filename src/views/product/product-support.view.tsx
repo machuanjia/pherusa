97b590dfb40dd5f3c6ee78bfbe713c473e829e3a
DayYoung <dayyoung@DayYoungdeMacBook-Pro.local>
2020-12-22 14:15:17 +0800

    feat: change some styles

--- src/views/product/product-support.view.tsx
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-import React, { Component } from 'react'
-
-export default class ProductSupportView extends Component {
-  render() {
-    return <div>ProductSupportView</div>
-  }
-}
--- src/views/product/product-trial.view.tsx
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-import React, { Component } from 'react'
-
-export default class ProductTrialView extends Component {
-  render() {
-    return <div>ProductTrialView</div>
-  }
-}
