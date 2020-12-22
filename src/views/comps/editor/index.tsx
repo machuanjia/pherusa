ca37c7a7440b3a3b9730b32678ceeb44eace73f5
DayYoung <dayyoung@DayYoungdeMacBook-Pro.local>
2020-12-22 13:37:17 +0800

    fix: add editor

--- src/views/comps/editor/index.tsx
+++ src/views/comps/editor/index.tsx
@@ -8,41 +8,41 @@ import { getHeight } from '@utils/index'
 
 interface IEditorProps {}
 interface IEditorState {
-    editorState: {}
-    outputHTML: string
-    height: number
+  editorState: {}
+  outputHTML: string
+  height: number
 }
 
 export default class EditorView extends Component<IEditorProps, IEditorState> {
-    constructor(props) {
-        super(props)
-        this.state = {
-            editorState: BraftEditor.createEditorState(''),
-            outputHTML: '',
-            height: 260,
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      editorState: BraftEditor.createEditorState(''),
+      outputHTML: '',
+      height: 260,
     }
-    componentDidMount() {
-        let _h = getHeight(100) - document.getElementsByClassName('bf-controlbar')[0].clientHeight
-        this.setState({
-            height: _h,
-        })
-    }
-    handleChange(editorState) {
-        this.setState({
-            editorState: editorState,
-            outputHTML: editorState.toHTML(),
-        })
-    }
-    render() {
-        const { editorState, height } = this.state
-        return (
-            <BraftEditor
-                contentStyle={{ height }}
-                className={styles['editor-wrap']}
-                value={editorState}
-                onChange={this.handleChange.bind(this)}
-            />
-        )
-    }
+  }
+  componentDidMount() {
+    let _h = getHeight(100) - document.getElementsByClassName('bf-controlbar')[0].clientHeight
+    this.setState({
+      height: _h,
+    })
+  }
+  handleChange(editorState) {
+    this.setState({
+      editorState: editorState,
+      outputHTML: editorState.toHTML(),
+    })
+  }
+  render() {
+    const { editorState, height } = this.state
+    return (
+      <BraftEditor
+        contentStyle={{ height }}
+        className={styles['editor-wrap']}
+        value={editorState}
+        onChange={this.handleChange.bind(this)}
+      />
+    )
+  }
 }
