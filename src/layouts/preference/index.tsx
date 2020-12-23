3704227b6e67c71e3540533f077212c368a29a7f
DayYoung <dayyoung@DayYoungdeMacBook-Pro.local>
2020-12-23 09:44:23 +0800

    feat: add preference menu

--- src/layouts/preference/index.tsx
+++ src/layouts/preference/index.tsx
@@ -1,16 +1,37 @@
 /** @format */
-import { Avatar } from 'laiye-antd'
+import { Avatar, Icon, Menu, Popover } from 'laiye-antd'
 import React, { Component } from 'react'
 import styles from './preference.module.less'
+import { logout } from '@utils/index'
 
 export default class PreferenceComponent extends Component {
   constructor(props) {
     super(props)
   }
+  menuAction({ key }) {
+    if (key === 'logout') {
+      logout()
+    }
+  }
   render() {
+    const content = (
+      <Menu style={{ width: 200 }} className={`${styles['prefernce-menu']}`} onClick={this.menuAction.bind(this)}>
+        <Menu.Item key="log">
+          <Icon type="question-circle" /> 更新日志
+        </Menu.Item>
+        <Menu.Item key="setting">
+          <Icon type="setting" /> 个人设置
+        </Menu.Item>
+        <Menu.Item key="logout">
+          <Icon type="logout" /> 退出
+        </Menu.Item>
+      </Menu>
+    )
     return (
       <div className={styles['preference-wrap']}>
-        <Avatar style={{ backgroundColor: '#87d068' }} icon="user" />
+        <Popover placement="rightBottom" title={null} content={content}>
+          <Avatar size="large" style={{ backgroundColor: '#79adf8' }} className="pointer" icon="user" />
+        </Popover>
       </div>
     )
   }
