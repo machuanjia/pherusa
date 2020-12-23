53952b3c519b313db2726b87d5e75c8d38d227be
DayYoung <dayyoung@DayYoungdeMacBook-Pro.local>
2020-12-23 14:55:25 +0800

    feat: add

--- src/components/bpmn/bpmn.editor.component.tsx
+++ src/components/bpmn/bpmn.editor.component.tsx
@@ -5,11 +5,13 @@ import { Button, Modal } from 'laiye-antd'
 import { MODAL_SIZE } from '@constants/index'
 import BpmnBasicComponent from './bpmn/basic.component'
 import BpmnEventComponent from './bpmn/event.component'
+import BpmnPaletteComponent from './bpmn/palette.component'
 
 interface IBpmnEditorProps {}
 interface IBpmnEditorState {
   isBasicVisible: boolean
   isEventVisible: boolean
+  isPaletteVisible: boolean
 }
 
 export default class BpmnEditorComponent extends Component<IBpmnEditorProps, IBpmnEditorState> {
@@ -18,6 +20,7 @@ export default class BpmnEditorComponent extends Component<IBpmnEditorProps, IBp
     this.state = {
       isBasicVisible: false,
       isEventVisible: false,
+      isPaletteVisible: false,
     }
   }
   setOpen(key) {
@@ -31,11 +34,12 @@ export default class BpmnEditorComponent extends Component<IBpmnEditorProps, IBp
     this.setState({
       isBasicVisible: false,
       isEventVisible: false,
+      isPaletteVisible: false,
     })
   }
 
   render() {
-    const { isBasicVisible, isEventVisible } = this.state
+    const { isBasicVisible, isEventVisible, isPaletteVisible } = this.state
     return (
       <div>
         <Button type="primary" onClick={this.setOpen.bind(this, 'isBasicVisible')}>
@@ -56,7 +60,7 @@ export default class BpmnEditorComponent extends Component<IBpmnEditorProps, IBp
           event
         </Button>
         <Modal
-          title="Basic"
+          title="Event"
           width={MODAL_SIZE.lg}
           footer={null}
           destroyOnClose={true}
@@ -64,6 +68,20 @@ export default class BpmnEditorComponent extends Component<IBpmnEditorProps, IBp
           onCancel={this.handleCancel.bind(this)}>
           <div style={{ height: '800px', overflow: 'auto' }}>
             <BpmnEventComponent />
+          </div>
+        </Modal>
+        <Button type="primary" className="m-l-12" onClick={this.setOpen.bind(this, 'isPaletteVisible')}>
+          palette
+        </Button>
+        <Modal
+          title="Palette"
+          width={MODAL_SIZE.lg}
+          footer={null}
+          destroyOnClose={true}
+          visible={isPaletteVisible}
+          onCancel={this.handleCancel.bind(this)}>
+          <div style={{ height: '800px', overflow: 'auto' }}>
+            <BpmnPaletteComponent />
           </div>
         </Modal>
       </div>
---
+++ src/components/bpmn/bpmn/palette.component.tsx
@@ -0,0 +1,62 @@
+/** @format */
+
+import React, { Component } from 'react'
+import Modeler from 'bpmn-js/lib/Modeler'
+
+import { pizzaBpmn } from './pizza.bpmn'
+import propertiesPanelModule from 'bpmn-js-properties-panel'
+import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda'
+import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda'
+
+export default class BpmnPaletteComponent extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  modeler = null
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.modeler = new Modeler({
+        container: '#bpmnview',
+        keyboard: {
+          bindTo: window,
+        },
+        propertiesPanel: {
+          parent: '#propview',
+        },
+        additionalModules: [propertiesPanelModule, propertiesProviderModule],
+        moddleExtensions: {
+          camunda: camundaModdleDescriptor,
+        },
+      })
+
+      this.newBpmnDiagram()
+    }, 1000)
+  }
+
+  newBpmnDiagram = () => {
+    this.openBpmnDiagram(pizzaBpmn)
+  }
+
+  openBpmnDiagram = xml => {
+    this.modeler.importXML(xml, error => {
+      if (error) {
+        return console.log('fail import xml')
+      }
+      const canvas = this.modeler.get('canvas')
+      canvas.zoom('fit-viewport')
+    })
+  }
+
+  render = () => {
+    return (
+      <div id="bpmncontainer">
+        <div
+          id="propview"
+          style={{ width: '15%', height: '98vh', float: 'right', maxHeight: '98vh', overflowX: 'auto' }}></div>
+        <div id="bpmnview" style={{ width: '85%', height: '98vh', float: 'left' }}></div>
+      </div>
+    )
+  }
+}
