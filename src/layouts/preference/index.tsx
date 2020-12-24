5b128d4f03ddcd4803a7b19a15ee6277adc8069e
DayYoung <dayyoung@DayYoungdeMacBook-Pro.local>
2020-12-24 09:44:33 +0800

    fix: change log

--- src/layouts/preference/index.tsx
+++ src/layouts/preference/index.tsx
@@ -1,18 +1,25 @@
 /** @format */
-import { Avatar, Drawer, Icon, Menu, Popover, Radio } from 'laiye-antd'
+import { Avatar, Drawer, Icon, Menu, Modal, Popover, Radio } from 'laiye-antd'
 import React, { Component } from 'react'
 import styles from './preference.module.less'
 import { logout } from '@utils/index'
 import { SketchPicker } from 'react-color'
+import { MODAL_SIZE } from '@constants/index'
 
 export default class PreferenceComponent extends Component {
   constructor(props) {
     super(props)
     this.state = {
       isSettingVisible: false,
+      isLogVisible: false,
     }
   }
   menuAction({ key }) {
+    if (key === 'log') {
+      this.setState({
+        isLogVisible: true,
+      })
+    }
     if (key === 'setting') {
       this.setState({
         isSettingVisible: true,
@@ -25,6 +32,11 @@ export default class PreferenceComponent extends Component {
   closeSetting() {
     this.setState({
       isSettingVisible: false,
+    })
+  }
+  closeLog() {
+    this.setState({
+      isLogVisible: false,
     })
   }
   changeLang(e) {
@@ -51,6 +63,35 @@ export default class PreferenceComponent extends Component {
         <Popover placement="rightBottom" title={null} content={content}>
           <Avatar size="large" style={{ backgroundColor: '#79adf8' }} className="pointer" icon="user" />
         </Popover>
+        <Modal
+          title="更新日志"
+          width={MODAL_SIZE.md}
+          visible={this.state['isLogVisible']}
+          footer={null}
+          onCancel={this.closeLog.bind(this)}>
+          <p>
+            <b>V0.0.1</b>
+            <div>本版本包含完整的项目框架结构，你可以稍加改动即可进行业务开发。包含内容如下：</div>
+          </p>
+          <p>1. http封装（采用axios插件做移除处理）</p>
+          <p>2. 国际化（i18next封装处理）</p>
+          <p>3. 导航</p>
+          <p>4. 布局（app布局和内容页布局）</p>
+          <p>5. 样式（变量定义及结构划分）</p>
+          <p>6. 状态管理</p>
+          <p>7. 路由（路由定义及鉴权逻辑）</p>
+          <p>8. 配置（项目配置，webpack配置）</p>
+          <p>9. mock数据</p>
+          <p>10. 代码风格</p>
+          <p>11. 代码检查（使用prettier和eslint）</p>
+          <p>12. 代码提交检查（commit-lint）</p>
+          <p>
+            <a href="https://wiki.laiye.com/display/FE/Pherusa" target="_blank">
+              详细文档
+            </a>
+          </p>
+        </Modal>
+
         <Drawer
           // @ts-ignore
           title="偏好设置"
