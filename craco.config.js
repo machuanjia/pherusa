c483aa3d85fb856e329e0a9b4606c6cc8da6a59a
DayYoung <dayyoung@DayYoungdeMacBook-Pro.local>
2021-04-22 19:25:24 +0800

    fix: change

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,58 @@
 const webpack = require('webpack')
 const CracoLessPlugin = require('craco-less')
 const path = require('path')
-const pathResolve = pathUrl => path.join(__dirname, pathUrl)
+
+const pathResolve = (pathUrl) => path.join(__dirname, pathUrl)
 const config = require('./server/config')
+
 const modifiedTheme = {
   '@laiye-primary-color': '#2249c0',
   '@laiye-link-color': '#2249c0',
+  '@color-primary': '#2249c0',
+  '@color-blue': '#79adf8',
+  '@color-black': '#000',
+  '@border-gray': '#d9dee2',
+  '@color-blue-dark': '#3f5361',
+  '@color-blue-normal': '#677a8e',
+  '@color-blue-light': '#8e9ca8',
+  '@color-light': '#f5f7f7',
+  '@color-danger': '#ff3e27',
+  '@color-warning': '#feb312',
+  '@color-success': '#37b184',
+  '@color-white': '#fff',
+  '@color-gray': '#666666',
+  '@color-light-gray': '#f0f2f5',
+  '@color-label-gray': '#425b6d',
+  '@color-normal-gray': '#687c8a',
+
+  // 功能划分
+  '@border': '@color-light',
+
+  '@icon': '@color-blue-light',
+  '@icon-white': '@color-white',
+  '@icon-dark': '@color-blue-dark',
+  '@icon-primary': '@color-primary',
+
+  '@text-white': '@color-white',
+  '@text-black': '@color-black',
+  '@text-primary': '@color-primary',
+  '@text-danger': '@color-danger',
+  '@text-success': '@color-success',
+  '@text-warning': '@color-warning',
+  '@text-desc': '@color-blue-normal',
+  '@text-empty': '@color-blue-light',
+  '@text-dark': '@color-blue-dark',
+
+  '@bg-primary': '@color-blue',
+  '@bg-warning': '@color-warning',
+  '@bg-main': '@color-light-gray',
+  '@bg-white': '@color-white',
+  '@table-shadow': '0 2px 8px 0 rgba(0, 0, 0, .1)',
+  '@shadow-gray': '#b3bdc5',
 }
 
-process.env.NODE_ENV === 'development' && (process.env.REACT_APP_CONFIGRATION = JSON.stringify(config))
+process.env.NODE_ENV === 'development' &&
+  (process.env.REACT_APP_CONFIGRATION = JSON.stringify(config))
 
 module.exports = {
   webpack: {
@@ -74,7 +118,7 @@ module.exports = {
     {
       plugin: CracoLessPlugin,
       options: {
-        modifyLessRule: function (lessRule, _context) {
+        modifyLessRule (lessRule, _context) {
           lessRule.test = /\.(less)$/
           lessRule.use = [
             {
@@ -106,7 +150,7 @@ module.exports = {
     port: config.env.port,
     proxy: {
       '/api': {
-        target: 'http://localhost:' + 3000,
+        target: `http://localhost:${  3000}`,
         changeOrigin: true,
       },
     },
--- src/routes/component.ts
+++ src/routes/component.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: D.Y
  * @Date: 2021-02-04 15:27:20
- * @LastEditTime: 2021-04-21 14:15:57
+ * @LastEditTime: 2021-04-22 19:25:10
  * @LastEditors: D.Y
  * @FilePath: /pherusa/src/routes/component.ts
  * @Description: 
@@ -25,15 +25,6 @@ const componentsRoute = {
       component: tableView,
       meta: {
         key: 'CList',
-        name: 'router.list',
-        iconType: 'list',
-      },
-    },
-    {
-      path: '/components/list2',
-      component: tableView,
-      meta: {
-        key: 'CList2',
         name: 'router.list',
         iconType: 'list',
       },
