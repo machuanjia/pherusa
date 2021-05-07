2c53be18db91f6bbee7f962c1821b2bb343d0e61
DayYoung <dayyoung@DayYoungdeMacBook-Pro.local>
2021-05-07 16:57:51 +0800

    fix: change pro

--- src/i18n/en/list.ts
+++ src/i18n/en/list.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: D.Y
  * @Date: 2021-01-05 14:38:36
- * @LastEditTime: 2021-04-27 15:42:19
+ * @LastEditTime: 2021-05-07 16:57:41
  * @LastEditors: D.Y
  * @FilePath: /pherusa/src/i18n/en/list.ts
  * @Description: 
@@ -9,6 +9,9 @@
 
 export default {
   name: 'Name',
+  namePlaceholder:'Please enter name',
+  description:'Description',
+  descriptionPlaceholder:'Please enter description',
   password: 'Password',
   address: 'Address',
   operation: 'Operation',
